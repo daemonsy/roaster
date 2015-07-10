7a6cdf8b25120cb88314df013e8d449f6cbc8fdd
Damon Aw <daemonsy@gmail.com>
2015-07-10 13:28:23 +0800

    Simple does it conversion of docs to PDF

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
+var path = require('path');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var shell = require('shelljs')
+var uuid = require('node-uuid');
+
+var libreBinaryLocation = process.env.LIBRE_BINARY_LOCATION || '/Applications/LibreOffice.app/Contents/MacOS/soffice';
 var port = process.env.PORT || 8888;
+var tmpFolder = path.resolve('./tmp');
 
-var wkhtmltopdf = require('wkhtmltopdf'),
-    io          = require('socket.io')();
 
-io.on('connection', function(socket) {
-  socket.on("render_pdf", function(data) {
-    buffers = [];
+shell.mkdir(tmpFolder);
 
-    data.options["disableJavascript"] = true;
-    content = "<html><body>" + data.content + "</body></html>";
-    console.log(content);
-    pdf = wkhtmltopdf(content, data.options);
+var express = require('express'),
+    http = require('http'),
+    compression = require('compression'),
+    util        = require('util');
+    multiparty = require('multiparty');
 
-    pdf.on('data', function(d) { buffers.push(d); });
+var app = express();
 
-    pdf.on('finish', function() {
-      var buffer = Buffer.concat(buffers);
-      socket.emit("rendered_pdf", { buffer: buffer });
+app.use(compression());
+
+app.post('/convert', function(request, response) {
+  var form = new multiparty.Form();
+  form.parse(request, function(error, fields, files) {
+    var fileUUID = uuid.v4();
+    var document = files.document[0];
+    var outputFileName = path.join(tmpFolder, fileUUID);
+    shell.mv(document.path, outputFileName + ".tmp");
+
+    exec(libreBinaryLocation + " --headless --convert-to pdf " + outputFileName + ".tmp" + " --outdir " + tmpFolder, function(error, output) {
+      if (error) throw error;
+      fs.readFile(outputFileName + ".pdf", function (error, data) {
+        if (error) throw error;
+        response.setHeader('Content-type', 'application/pdf');
+        response.end(data);
+      });
     });
   });
 });
 
-io.listen(port);
-
-console.log('Started listening on port ' + port);
+app.listen(port);
